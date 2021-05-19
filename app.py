41c4d1b589e364435adcfc6df22674fe07c70f5a
mbelousov <belousov.mik@gmail.com>
2021-05-19 13:33:37 +0200

    FIX: fixed error with wrong import.

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 from aiogram import executor, types
 
 # from utils.notify_admins import on_startup_notify
-from loader import dp, bot
+from handlers import dp
 
 
 #
 # async def on_startup(dispatcher):
 #     await on_startup_notify(dispatcher)
-
-@dp.message_handler(content_types='text', state=None)
-async def bot_echo(message: types.Message):
-    chat_id = message.chat.id
-    text = message.text
-    print(text)
-    await bot.send_message(chat_id=chat_id, text=text)
-
-    await message.answer(f'{text} from answer alice')
-    await message.reply(f'{text} from reply alice')
+#
 
 if __name__ == '__main__':
+
     executor.start_polling(dp)
