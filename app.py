ea2fc912bb5dc51d6de6d4d5e8a140b6c1f565d3
belousov <tobigface@gmail.com>
2021-06-20 21:03:05 +0200

    FIX: fixed middleware

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from aiogram import executor
 
 import middlewares, filters, handlers   # Порядок импортов важен!
 from loader import dp
+from middlewares import BigBrother
 
 from utils.notify_admins import on_startup_notify
 
@@ -11,4 +12,5 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
+    dp.middleware.setup(BigBrother())
     executor.start_polling(dp, on_startup=on_startup)
