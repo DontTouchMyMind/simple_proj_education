0e8c7bd8eb9a326855609438e7c6b87547d36b93
belousov <tobigface@gmail.com>
2021-06-22 07:21:14 +0200

    WIP: continue education Middleware.

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -4,7 +4,7 @@ from loader import dp
 
 
 @dp.message_handler(content_types='text', state=None)
-async def bot_echo(message: types.Message):
+async def bot_echo(message: types.Message, middleware_data):
     chat_id = message.chat.id
     text = message.text
     print(text)
@@ -12,3 +12,5 @@ async def bot_echo(message: types.Message):
 
     await message.answer(f'{text} from answer alice')
     await message.reply(f'{text} from reply alice')
+    print(f"Middleware data = {middleware_data}")
+    await message.answer(f"From middleware date {middleware_data}")
--- middlewares/big_brothers.py
+++ middlewares/big_brothers.py
@@ -9,12 +9,14 @@ from data.config import banned_users
 
 # В этом мидлваре можно делать запрос к базе данных, напр. достать список заблокированных юзеров
 class BigBrother(BaseMiddleware):
-    # on_point_event_type
+    # 1
+    # on_<point>_<event_type>
     async def on_pre_process_update(self, update: types.Update, data: dict):
-        print('ppu')
+
         logging.info("[------------New update-------------]")
         logging.info("1.Pre Process Update")
         data['PPU'] = "It's some data was added in PreProcessUpdate"
+        print(data)
         logging.info(f"Update = {update}")
         logging.info("The next step is ProcessUpdate")
 
@@ -28,6 +30,7 @@ class BigBrother(BaseMiddleware):
         if user in banned_users:
             raise CancelHandler()
 
+    # 2
     async def on_process_update(self, update: types.Update, data: dict):
         logging.info("2. Process Update")
         logging.info(f"Data from Pre Process Message {data}")
@@ -35,8 +38,30 @@ class BigBrother(BaseMiddleware):
         logging.info(f"Update = {update}")
         logging.info("The next step is PreProcessMessage")
 
+    # 3
     async def on_pre_process_message(self, message: types.Message, data: dict):
         logging.info("3.Pre Process Message")
         logging.info(f"Input data = {data}")
         logging.info("The next step is Filters -> ProcessMessage")
         data['PPM'] = "It's some data was added in PreProcessMessage"
+
+    # 4 Filters
+
+    # 5
+    async def on_process_message(self, message: types.Message, data: dict):
+        logging.info("5. Process Message")
+        logging.info(f"Input data = {data}")
+        logging.info("The next step is Filters -> ProcessMessage")
+        data['PM'] = "It's some data was added in ProcessMessage"
+        print(f"data from PM = {data}")
+        new_data = {
+            'PM': data.get('PM'),
+            'PPM': data.get('PPM')
+        }
+        data['middleware_data'] = new_data
+        # Data from PM mb send to events handler
+        # data from PM = {
+        #   'PPM': "It's some data was added in PreProcessMessage",
+        #   'state': < aiogram.dispatcher.storage.FSMContext object at 0x7fc9b10d7790 >,
+        #   'raw_state': None,
+        #   'PM': "It's some data was added in ProcessMessage"}
