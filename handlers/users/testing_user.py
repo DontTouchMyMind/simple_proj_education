c8c15ede16737ba9b3531395ae235f3a392c56ea
belousov <tobigface@gmail.com>
2021-06-26 11:03:35 +0200

    ADD: connected Redis as FSM.

--- handlers/users/testing_user.py
+++ handlers/users/testing_user.py
@@ -8,7 +8,10 @@ from states import Testing
 
 
 @dp.message_handler(Command('test'), state=None)
-async def enter_test(message: types.Message):
+async def enter_test(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    await message.answer(str(data))
+
     await message.answer("You starting to test\n Question #1\nWhat's your name?")
     await Testing.Q1.set()
 
@@ -39,7 +42,8 @@ async def answer_q2(message: types.Message, state: FSMContext):
     data = await state.get_data()
     answer1 = data.get('answer1')
     answer2 = message.text
-
+    async with state.proxy() as data:
+        data['answer2'] = answer2
     await message.answer(f"Thx for your answers!\n"
                          f"Your answers are:\n"
                          f"{answer1=}\n"
@@ -49,6 +53,6 @@ async def answer_q2(message: types.Message, state: FSMContext):
     # мы должны его сбросить
     # Есть несколько вариантов как это можно реализовать:
     # Сбрасываются состояние и сбрасываются данные
-    await state.finish()    # Это самый простой вариант
+    # await state.finish()    # Это самый простой вариант
     # await state.reset_state()   # Также сбрасываются состояние и данные
-    # await state.reset_state(with_data=False)    # Сбросит состояние, но оставит данные в state.get_data!
+    await state.reset_state(with_data=False)    # Сбросит состояние, но оставит данные в state.get_data!
