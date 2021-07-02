0c6a107f59c4ac5d35edcf40380ff64ccfc9af08
Mikhail Belousov <belousov.mik@gmail.com>
2021-07-02 14:09:44 +0200

    WIP

--- handlers/users/purchase.py
+++ handlers/users/purchase.py
@@ -1,7 +1,12 @@
+import logging
+
 from aiogram import types
 from aiogram.dispatcher.filters import Command
+from aiogram.types import CallbackQuery
 
-from loader import dp
+from keybords.inline.callback_datas import buy_callback
+from keybords.inline.choice_buttons import choice
+from loader import dp, bot
 
 
 @dp.message_handler(Command('item'))
@@ -11,3 +16,13 @@ async def show_items(message: types.Message):
              f"If you don't need anything click cancel.",
         reply_markup=choice
     )
+
+
+# Отлавливаем нажатие кнопки Купи Грушу
+@dp.message_handler(buy_callback.filter(item_name='pear'))
+async def buying_pear(call: CallbackQuery, callback_data: dict):    # callback_data - создается фильтром buy_callback.filter
+    # await bot.answer_callback_query(callback_query_id=call.id)     # Закрываем часики!
+    # Или так
+    await call.answer(cache_time=60)
+    logging.info(f"callback_data = {call.data}")
+    logging.info(f"callback_data DICT = {callback_data}")
--- keybords/inline/choice_buttons.py
+++ keybords/inline/choice_buttons.py
@@ -1,11 +1,26 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from keybords.inline.callback_datas import buy_callback
 
 choice = InlineKeyboardMarkup(
     row_width=2,
     inline_keyboard=[
         [
-
+            InlineKeyboardButton(
+                text='Купи грушу',
+                callback_data=buy_callback.new(
+                    item_name='pear',
+                    quantity=1
+                )
+            ),
+            InlineKeyboardButton(
+                text='Купи яблоки',
+                callback_data='buy:apple:5'     # Расшифровка buy_callback.new()
+            ),
+            InlineKeyboardButton(
+                text='Отмена',
+                callback_data='cancel'
+            )
         ]
     ]
 )
