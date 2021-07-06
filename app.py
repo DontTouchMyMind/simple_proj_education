6e1b057857b5be5894812c36432235d0a441f3c1
Mikhail Belousov <belousov.mik@gmail.com>
2021-07-06 10:21:18 +0200

    ADD: added defaults commands.

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import middlewares, filters, handlers   # Порядок импортов важ
 from loader import dp
 
 from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
+    await set_default_commands(dispatcher)
 
 
 if __name__ == '__main__':
